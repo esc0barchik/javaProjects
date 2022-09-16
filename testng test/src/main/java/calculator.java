6b31f8adc8994928339e459ec42d79795b960ad5
esc0barchik <89536720+esc0barchik@users.noreply.github.com>
2022-09-16 15:42:25 +0200

    Add files via upload
    Пробное написание автотестов на TestNG для маленькой программы.

---
+++ testng test/src/main/java/calculator.java
@@ -0,0 +1,18 @@
+public class calculator {
+    public double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public double sub(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public double mult(double num1, double num2) {
+
+        return num1 * num2;
+    }
+
+    public double div(double num1, double num2) {
+        return num1 / num2;
+    }
+}
---
+++ testng test/src/test/java/PriorityTests.java
@@ -0,0 +1,38 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PriorityTests {
+    private calculator calculator = new calculator();
+    private final double num1 = 10;
+    private final double num2 = 5;
+@Test(priority = 3)
+    public void checkAdd(){
+        double expectedResult = num1 + num2;
+        double actualResult = calculator.add(num1,num2);
+
+        Assert.assertEquals(actualResult, expectedResult, "test failed for method add");
+    }
+    @Test(priority = 1)
+    public void checkMult(){
+        double expectedResult = num1 * num2;
+        double actualResult = calculator.mult(num1,num2);
+
+        Assert.assertEquals(actualResult, expectedResult, "test failed for method multiplication");
+    }
+    @Test(priority = 4)
+    public void checkDiv(){
+        double expectedResult = num1 / num2;
+        double actualResult = calculator.div(num1,num2);
+
+        Assert.assertEquals(actualResult, expectedResult, "test failed for method division");
+    }
+    @Test
+    public void checkSub(){
+        double expectedResult = num1 - num2;
+        double actualResult = calculator.sub(num1,num2);
+
+        Assert.assertEquals(actualResult, expectedResult, "test failed for method substacrion");
+    }
+
+
+}
